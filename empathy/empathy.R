56a26890c9967f5f774fd8d87d7afd260cdf41d5
Andrew Grogan-Kaylor <agrogan@m-m0vy37d2td.local>
2026-01-06 09:45:56 -0500

    FIRST COMMIT

---
+++ empathy/empathy.R
@@ -0,0 +1,44 @@
+# try for 3d RGL of model fit
+
+# libraries
+
+library(haven) # read Stata
+
+# library(brms) # Bayes
+
+library(sjPlot) # nice tables inc. for Bayes
+
+library(labelled) # work w labels
+
+library(ggeffects) # model effects
+
+library(ggplot2) # beautiful graphs
+
+# get data
+
+empathy <- read_dta("empsui_043024.dta")
+
+mod_gam1 <- gam(N_SUTHO ~ s(empathy) + maltreat +
+                  sexab + A_AGE + zses, 
+                family = binomial(link="logit"), # logistic regression
+                data = empathy)
+
+tab_model(mod_gam1)
+
+plot(mod_gam1) # plot of smoother term(s) in fit
+
+mydf <- ggpredict(mod_gam1, 
+                  terms = c("empathy",
+                            "maltreat")) # dataframe of fit
+
+with(mydf, plot3d(x, 
+                  group, 
+                  predicted, 
+                  type="s",
+                  col = as.numeric(group),
+                  xlab = "empathy",
+                  ylab = "maltreatment",
+                  zlab = "predicted probability"))
+
+# with(iris, plot3d(Sepal.Length, Sepal.Width, Petal.Length, 
+#                   type="s", col=as.numeric(Species)))
