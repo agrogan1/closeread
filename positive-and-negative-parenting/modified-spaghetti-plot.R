371a3f6a01104d9cd081dad82bd56b2cf40f13d6
Andrew Grogan-Kaylor <agrogan@m-m0vy37d2td.local>
2026-03-07 08:27:35 -0500

    FIRST COMMIT

---
+++ positive-and-negative-parenting/modified-spaghetti-plot.R
@@ -0,0 +1,15 @@
+library(haven)
+
+reffects <- read_dta("positive-and-negative-parenting/reffects.dta")
+
+library(ggplot2)
+
+ggplot(reffects,
+       aes(color = country)) +
+  geom_segment(aes(x = 0,
+                   xend = 1,
+                   y = 0,
+                   yend = (.023 + u5) * 1)) +
+  theme(legend.position = "none")
+
+
