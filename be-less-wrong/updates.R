a267d2941fd1aa7d2eaaf35094c55ff97d9531da
Andrew Grogan-Kaylor <agrogan@umich.edu>
2026-04-09 14:54:28 -0400

    Add script to sort bibliography and fix accents
    This script sorts the bibliography and fixes strings with Spanish accents in the specified .bib file.

---
+++ be-less-wrong/updates.R
@@ -0,0 +1,27 @@
+# sort bibliography
+
+library(RefManageR)
+
+mybib <- ReadBib("./be-less-wrong.bib")
+
+mybib <- sort(mybib, sorting = "nyt") # sort by name - year - title
+
+WriteBib(mybib, file = "./be-less-wrong.bib")
+
+# fix strings with Spanish accents
+# because RefManageR somehow kludges them
+
+# bibliography  <- readLines("./be-less-wrong.bib")
+
+# bibliography  <- gsub(pattern = "Ignacio Mart{\\a'\\i}n-Bar{\\a'o}", 
+#                       replace = "Ignacio Martín-Baró", 
+#                       x = bibliography,
+#                       fixed = TRUE)
+
+# bibliography  <- gsub(pattern = "Ellacur{\\a'\\i}a", 
+#                       replace = "Ellacuría", 
+#                       x = bibliography,
+#                       fixed = TRUE)
+
+
+# writeLines(bibliography, con="be-less-wrong.bib")
