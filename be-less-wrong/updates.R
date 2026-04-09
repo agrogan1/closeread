c88b7137cb80399910852456920243e39e65dfdb
Andrew Grogan-Kaylor <agrogan@m-m0vy37d2td.local>
2026-04-09 15:11:16 -0400

    added cites about Simpson's paradox; sorted bibliography and fixed accents in bibliography

--- be-less-wrong/updates.R
+++ be-less-wrong/updates.R
@@ -2,21 +2,21 @@
 
 library(RefManageR)
 
-mybib <- ReadBib("./be-less-wrong.bib")
+mybib <- ReadBib("./be-less-wrong/be-less-wrong.bib")
 
 mybib <- sort(mybib, sorting = "nyt") # sort by name - year - title
 
-WriteBib(mybib, file = "./be-less-wrong.bib")
+WriteBib(mybib, file = "./be-less-wrong/be-less-wrong.bib")
 
 # fix strings with Spanish accents
 # because RefManageR somehow kludges them
 
-# bibliography  <- readLines("./be-less-wrong.bib")
+bibliography  <- readLines("./be-less-wrong/be-less-wrong.bib")
 
-# bibliography  <- gsub(pattern = "Ignacio Mart{\\a'\\i}n-Bar{\\a'o}", 
-#                       replace = "Ignacio Martín-Baró", 
-#                       x = bibliography,
-#                       fixed = TRUE)
+bibliography  <- gsub(pattern = "Ignacio Mart{\\a'\\i}n-Bar{\\a'o}",
+                      replace = "Ignacio Martín-Baró",
+                      x = bibliography,
+                      fixed = TRUE)
 
 # bibliography  <- gsub(pattern = "Ellacur{\\a'\\i}a", 
 #                       replace = "Ellacuría", 
@@ -24,4 +24,4 @@ WriteBib(mybib, file = "./be-less-wrong.bib")
 #                       fixed = TRUE)
 
 
-# writeLines(bibliography, con="be-less-wrong.bib")
+writeLines(bibliography, con="./be-less-wrong/be-less-wrong.bib")
